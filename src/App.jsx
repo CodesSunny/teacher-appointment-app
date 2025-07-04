aa29f0c30b4c157c7da25c1ad81e558c59d3f74c
TechSunny <sunnymotivation2@gmail.com>
2025-07-04 17:53:08 +0530

    fixed signup routing issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom";
 import Login from './pages/Login';
 import AdminDashboard from './pages/admin/Dashboard';
-import AdminSignUp from './pages/admin/SignUp';
 import TeacherDashboard from './pages/teacher/Dashboard';
-import TeacherSignUp from './pages/admin/SignUp';
 import StudentDashboard from './pages/student/Dashboard';
-import StudentSignUp from './pages/admin/SignUp';
+import AdminSignUp from './pages/admin/SignUp';
+import TeacherSignUp from './pages/teacher/SignUp';
+import StudentSignUp from './pages/student/SignUp';
 import Home from './pages/navLinks/Home';
 import Services from './pages/navLinks/Services';
 import Blog from './pages/navLinks/Blog';
@@ -30,7 +30,7 @@ function App() {
         <Route path="/teacher/signup" element={<TeacherSignUp />} />
         <Route path="/student/dashboard" element={<StudentDashboard />} />
         <Route path="/student/signup" element={<StudentSignUp />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/services" element={<Services />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/contact" element={<Contact />} />
