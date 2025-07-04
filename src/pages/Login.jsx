bcd43a9b10437109c63d225042069ba774c39eda
TechSunny <sunnymotivation2@gmail.com>
2025-07-04 12:11:43 +0530

    fixed responsiveness og login ui

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -48,15 +48,16 @@ const Login =()=>{
                     height: '100vh',
                     width: '100vw',
                     display:'flex',
+                    overflow:'hidden'
                     }} >
 
                         {/* left side for ..role select */}
             <section className="basis-3/12 h-screen  bg-gray-200 flex flex-col justify-center items-center gap-8 animate__animated animate__slideInLeft">
-                <label className="w-48 text-center text-lg font-semibold bg-gray-300 p-4 rounded">Select Role:</label>
+                <label className="w-36 md:w-48 text-center text-md md:text-lg font-semibold bg-gray-300 p-4 rounded">Select Role:</label>
                 <select
                      value={role}
                      onChange={(e)=> setRole(e.target.value)}
-                     className="w-48 text-center text-lg font-semibold bg-gray-300 p-4 rounded">
+                     className="w-36 md:w-48 text-center text-lg font-semibold bg-gray-300 p-4 rounded">
                     <option value="">-- Select --</option>
                     <option value="admin" >Admin</option>
                     <option value="teacher" >Teacher</option>
@@ -73,7 +74,7 @@ const Login =()=>{
                         }
                          
                     }}
-                    className='bg-blue-600 text-white px-6 py-2 rounded-lg text-white text-xl font-semibold shadow-lg hover:cursor-pointer hover:scale-95'
+                    className='bg-blue-600 text-white px-4 md:px-6 py-2 rounded-lg text-white text-md md:text-xl font-semibold shadow-lg hover:cursor-pointer hover:scale-95'
                  >
                     {role ? (
                               `${role.charAt(0).toUpperCase() + role.slice(1)} Login`
@@ -84,48 +85,52 @@ const Login =()=>{
 
 
             {/* ---------................form container............................. */}
-            <section className=" basis-9/12 h-screen rounded-lg p-8  flex justify-center items-center animate__animated animate__zoomIn">
-
+            <section className=" basis-9/12 h-screen rounded-lg p-8  flex flex-col justify-center items-center animate__animated animate__zoomIn">
+                    
                         {/* conditiona rendering :single form for all roles  */}
 
                         {confirmedRole && (    //ensure form renders after btn clicked
-                                <form
+                            <div className=' bg-gray-200 px-8 py-6 rounded-lg shadow-lg shadow-blue-400'>
+                               <form
                                     onSubmit={handleSubmit}
-                                    action="#" className='min-w-[550px] bg-gray-200 py-6 flex flex-col items-center gap-4 rounded-lg '>
+                                    action="#" className='flex flex-col items-center gap-4  '>
        
-                               <h1 className=' text-blue-600 w-full text-center text-xl font-bold py-2'>{confirmedRole.charAt(0).toUpperCase() + confirmedRole.slice(1)} Login </h1>
-                               <hr className='bg-black w-full'/>
-       
-                               <div className='flex flex-col gap-2 w-96 '>
-                                    <label htmlFor="username" className='text-lg'>Username:</label>
-                                    <input
-                                        onChange={handleChange}
-                                        onBlur={handleBlur}
-                                        value={values.uname}
-                                        name='uname' 
-                                        type="username" id='username' placeholder='techsunny'
-                                        className='py-2 px-4 rounded bg-gray-300'/>
-                                    <span className='text-red-500'> {touched.uname && errors.uname }</span>
-                               </div>
-                               
-                               <div className='flex flex-col gap-2 w-96 '>
-                                    <label htmlFor="password" className='text-lg'>Password:</label>
-                                    <input
-                                        onChange={handleChange}
-                                        onBlur={handleBlur}
-                                        value={values.password}
-                                        name='password' 
-                                        type="password" id='password' placeholder='***' minLength={5}
-                                        className='py-2 px-4 rounded bg-gray-300'/>
-                                    <span className='text-red-500'>{touched.password && errors.password} </span>
-                               </div>
+                                    <h1 className=' text-blue-600 w-full text-center text-xl font-bold py-2'>{confirmedRole.charAt(0).toUpperCase() + confirmedRole.slice(1)} Login </h1>
+                                    <hr className='bg-black w-full'/>
+            
+                                    <div className='flex flex-col gap-2 w-48 sm:w-72 md:w-96 '>
+                                            <label htmlFor="username" className='text-md md:text-lg'>Username:</label>
+                                            <input
+                                                onChange={handleChange}
+                                                onBlur={handleBlur}
+                                                value={values.uname}
+                                                name='uname' 
+                                                type="username" id='username' placeholder='techsunny'
+                                                className='py-2 px-4 rounded bg-gray-300'/>
+                                            <span className='text-red-500'> {touched.uname && errors.uname }</span>
+                                    </div>
+                                    
+                                    <div className='flex flex-col gap-2 w-48 sm:w-72 md:w-96 '>
+                                            <label htmlFor="password" className='text-md md:text-lg'>Password:</label>
+                                            <input
+                                                onChange={handleChange}
+                                                onBlur={handleBlur}
+                                                value={values.password}
+                                                name='password' 
+                                                type="password" id='password' placeholder='***' minLength={5}
+                                                className='py-2 px-4 rounded bg-gray-300'/>
+                                            <span className='text-red-500'>{touched.password && errors.password} </span>
+                                    </div>
 
-                               <button type="submit" className='bg-blue-600 text-white px-12 py-4 rounded-lg text-white text-xl font-semibold shadow-lg hover:cursor-pointer hover:scale-95'>Submit </button>
+                                    <button type="submit" className='bg-blue-600 text-white px-4 md:px-12 py-2 md:py-4 rounded-lg text-white md:text-xl font-semibold shadow-lg hover:cursor-pointer hover:scale-95'>Submit </button>
                                </form>
+
+                                            {/* when user not registered : sign up form*/}
+                                <p className='mt-4 text-center'>Not a registered user ? <span className='text-blue-800 hover:cursor-pointer hover:font-semibold'> Signup here </span></p>
+                            </div>
                         )}
 
-                    
-                    
+                          
                     
 
             </section>
